ac63130cd1012cdb08c62d8323aa7db7e3bd85ec
Zain2012-byte <muzaffartawseefzain@gmail.com>
2024-12-06 20:42:02 +0530

    lesson.17

---
+++ break.py
@@ -0,0 +1,7 @@
+#Taking an input from the user
+a= input("Enter a word : ")
+for i in a:
+    if (i=='a'):
+        print("a is found")
+    else:
+        print("a is not found")
---
+++ continue.py
@@ -0,0 +1,9 @@
+var=10
+
+while var>0:
+    var-=1
+    if var==5:
+        continue
+    print("\n The current variable is",var)
+
+print("\n Good bye")
